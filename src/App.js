6e7f9ad68d25aa2f48a5fb3accec23721d721ee9
gsim2345 <gb@gabriellasimon.me>
2019-02-24 08:42:42 +0100

    Added new Blog container, and moved Posts, NewPosts, FullPosts in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
@@ -6,9 +7,11 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
