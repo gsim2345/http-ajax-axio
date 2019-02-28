22aa965cf019daa98660be34cab03d23b9c8d7d1
gsim2345 <gb@gabriellasimon.me>
2019-02-28 17:45:46 +0100

    removed withRouting component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-
 import './Post.css';
 
-const post = (props) => {
-    // routing related props are not passed down the component three
-    console.log(props);
-    return(
+const post = (props) => (
         <article className="Post" onClick={props.clicked}>
-        <h1>{props.title}</h1>
-        <div className="Info">
-            <div className="Author">{props.author}</div>
-        </div>
-    </article>
-    )
-}
+            <h1>{props.title}</h1>
+            <div className="Info">
+                <div className="Author">{props.author}</div>
+            </div>
+        </article> )
     
-export default withRouter(post);
+
+    
+export default post;
