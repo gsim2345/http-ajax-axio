863b90fc0485e797df523f8697d8a1e362478a16
gsim2345 <gb@gabriellasimon.me>
2019-03-08 14:13:12 +0100

    Explanation for adding basename to router if app served from subdirectory

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // the server needs to be configured, that it always forwards requests, and the router handles the request, even if it's 404
+      //if we are serving the server from a subdirectory, and not only from "/", it needs to be set in the router:
+      //<BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
